9f9f251f5bcc27c4bce76f9aa847b9326384d258
Steven Hangger <LonghronShen@Hotmail.com>
2023-03-14 06:12:11 +0000

    build: use clang-cl toolchain

--- noCrtTemplate/ncrt_memory.cpp
+++ noCrtTemplate/ncrt_memory.cpp
@@ -5,19 +5,28 @@
 
 extern "C"
 {
-	#pragma function(memset)
+#pragma function(memset)
 	void* __cdecl memset(void* dest, int value, size_t num)
 	{
+#ifdef __clang__
+		size_t len = num;
+		unsigned char* p = (unsigned char*)dest;
+		while (len--)
+		{
+			*p++ = (unsigned char)value;
+		}
+#else
 		__stosb(static_cast<unsigned char*>(dest),
-				static_cast<unsigned char>(value), num);
+			static_cast<unsigned char>(value), num);
+#endif
 		return dest;
 	}
 
-	#pragma function(memcpy)
+#pragma function(memcpy)
 	void* __cdecl memcpy(void* dest, const void* src, size_t num)
 	{
-		__movsb(static_cast<unsigned char*>(dest), 
-				static_cast<const unsigned char*>(src), num);
+		__movsb(static_cast<unsigned char*>(dest),
+			static_cast<const unsigned char*>(src), num);
 		return dest;
 	}
 }
@@ -32,12 +41,12 @@ void* operator new[](size_t n)
 	return HeapAlloc(GetProcessHeap(), NULL, n);
 }
 
-void operator delete(void * p)
+void operator delete(void* p)
 {
 	HeapFree(GetProcessHeap(), NULL, p);
 }
 
-void operator delete[](void * p)
+void operator delete[](void* p)
 {
 	HeapFree(GetProcessHeap(), NULL, p);
 }
